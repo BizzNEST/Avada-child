25860e4e0b9d59fe4f5eb12f2c5dd5375dba8074
Joel Mariscal <jomarisc@ucsc.edu>
2025-11-20 14:13:52 -0800

    feat(hero-description):added desc column

--- templates/template-parts/eoy-hero.php
+++ templates/template-parts/eoy-hero.php
@@ -1,11 +1,15 @@
 <section class="eoy-hero">
     <div class="container">
-        <h2>Hero Section</h2>
-        <p> 
-            This is a pivotal time for Digital NEST. As we equip young people with the 
-            power to reshape their futures and create economic prosperity for families, 
-            we will become a national model for workforce development and sustain our 
-            presence for generations to come.
-        </p>
+        <div class='hero-marquee'>Marquee Section</div>
+        <div class="hero-description">
+            <h2>Your impact in 2025</h2>
+            <p>
+                This is a pivotal time for Digital NEST. As we equip young people with 
+                the power to reshape their futures and create economic prosperity for 
+                families, we will become a national model for workforce development 
+                and sustain our presence for generations to come.
+            </p>
+            <button class='btn-donation'>DONATE NOW</button>
+        </div>
     </div>
 </section>
