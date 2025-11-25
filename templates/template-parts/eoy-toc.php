1457dc5e57e7b3012d0c0a320bc63e0742139981
Luciano Lorenzana <lucianolomalor251@gmail.com>
2025-11-25 14:02:23 -0800

    feat(toc): setup skeleton for table of contents

--- templates/template-parts/eoy-toc.php
+++ templates/template-parts/eoy-toc.php
@@ -1,5 +1,15 @@
 <section class="eoy-toc">
     <div class="container">
-        <h2>Table of Contents</h2>
+        <h2>The future starts with you, will you support the NEST generation?</h2>
+
+        <div>
+            <button> Economic Prosperity</button>
+            <button> Confidence & Agency </button>
+            <button> A Workforce Launchpad </button>
+            <button> Financial Foundation </button>
+        </div>
+
+        
     </div>
+    
 </section>
