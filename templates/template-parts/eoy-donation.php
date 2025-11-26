0806bd06baa0b8d494f0e07082c0fb4f5529b856
Rene Rodriguez <renerodriguez@admins-MacBook-Pro.local>
2025-11-26 11:52:54 -0800

    type(donations):created a skeletal framework for donations

--- templates/template-parts/eoy-donation.php
+++ templates/template-parts/eoy-donation.php
@@ -1,5 +1,17 @@
 <section class="eoy-donation">
     <div class="container">
-        <h2>Donate Now!</h2>
+        <div class="hero">
+            <div class="text">
+                <h3>
+                    Give today and support the NEST-Generation!
+                </h3>
+                <button class="donate">
+                    Donate Now
+                </button>
+            </div>
+        </div>
+        <div class="subtext">
+            <p>Crafted, designed, and developed by BizzNEST Associates © 2025.</p>
+        </div>
     </div>
 </section>
