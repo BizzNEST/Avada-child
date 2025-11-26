3c1bbd495a6e601f6a58cf49fb7469870e215fbc
NushK650 <Nush8653@gmail.com>
2025-11-26 12:43:31 -0800

    feat(confidence): I created the basic skeleton of the confidence section

--- templates/template-parts/eoy-confidence.php
+++ templates/template-parts/eoy-confidence.php
@@ -1,5 +1,22 @@
 <section class="eoy-confidence">
     <div class="container">
-        <h2>Confidence and Agency</h2>
+
+        <img src="/Property 1=Variant2.png" alt="2nd Star">
+        <h2>Confidence & Agency</h2>
+        <div>
+            <div>Confidence is necessary to identify, pursue, and achieve one’s professional ambitions. At Digital NEST, we bake confidence into our programming within four categories: career navigation skills, professional skills, technical skills, and (for our 18+ members) securing a next step that prepares them for a wealth-building career such as a job, internship, or enrollment or return to college. </div>
+            <div>This year, our members reported dramatic increases in their ability to navigate their futures. When NESTers exercise their agency to follow their dreams, transformation begins.</div>
+        </div>
+        <div><li>62% increased confidence that they can identify employers, companies, and/or institutions relevant to their career goals.
+            47% increased confidence that they can find accurate and helpful information about careers they are interested in.
+            55% increased confidence that they can define how they want to establish and maintain a work-life balance.
+            53% increased confidence they can determine the steps they need to take to successfully accomplish their education and/or career goals
+            42% increased confidence they can choose a career that will match their work-life balance.</li></div>
+        <div>
+            <img src="/Image.png" alt="">
+            <img src="/Image (1).png" alt="">
+            <img src="/Image (2).png" alt="">
+            <img src="/Image (3).png" alt="">
+        </div>
     </div>
 </section>
