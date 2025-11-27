59f62f0112c4ad9b625a199c681045da98fbdadc
NushK650 <Nush8653@gmail.com>
2025-11-26 16:56:13 -0800

    feat(confidence): finished skeleton, worked on css, still need to do animation

--- templates/template-parts/eoy-confidence.php
+++ templates/template-parts/eoy-confidence.php
@@ -1,22 +1,39 @@
 <section class="eoy-confidence">
     <div class="container">
-
-        <img src="/Property 1=Variant2.png" alt="2nd Star">
-        <h2>Confidence & Agency</h2>
-        <div>
-            <div>Confidence is necessary to identify, pursue, and achieve one’s professional ambitions. At Digital NEST, we bake confidence into our programming within four categories: career navigation skills, professional skills, technical skills, and (for our 18+ members) securing a next step that prepares them for a wealth-building career such as a job, internship, or enrollment or return to college. </div>
-            <div>This year, our members reported dramatic increases in their ability to navigate their futures. When NESTers exercise their agency to follow their dreams, transformation begins.</div>
+        <div class='layer-one'>
+            <img class="star" src="<?= get_stylesheet_directory_uri() ?>/assets/images/eoy-2025/Stars/Property 1=Variant2.png" alt="2nd Star">
+            <h2 class="confidence-title">Confidence & Agency</h2>
         </div>
-        <div><li>62% increased confidence that they can identify employers, companies, and/or institutions relevant to their career goals.
-            47% increased confidence that they can find accurate and helpful information about careers they are interested in.
-            55% increased confidence that they can define how they want to establish and maintain a work-life balance.
-            53% increased confidence they can determine the steps they need to take to successfully accomplish their education and/or career goals
-            42% increased confidence they can choose a career that will match their work-life balance.</li></div>
-        <div>
-            <img src="/Image.png" alt="">
-            <img src="/Image (1).png" alt="">
-            <img src="/Image (2).png" alt="">
-            <img src="/Image (3).png" alt="">
+        <div class="layer-two">
+            <div class="layer-two-left">
+                <div class="top-left-textbox">Confidence is necessary to identify, pursue, and achieve one’s professional ambitions. At Digital NEST, we bake confidence into our programming within four categories: career navigation skills, professional skills, technical skills, and (for our 18+ members) securing a next step that prepares them for a wealth-building career such as a job, internship, or enrollment or return to college. </div>
+                <div class='bottom-left-textbox'>This year, our members reported dramatic increases in their ability to navigate their futures.<span class="bold"> When NESTers exercise their agency to follow their dreams, transformation begins.</span></div>
+            </div>
+            <div class="layer-two-right">
+                <li>62% increased confidence that they can identify employers, companies, and/or institutions relevant to their career goals.</li>
+                <li> 47% increased confidence that they can find accurate and helpful information about careers they are interested in.</li>
+                <li>55% increased confidence that they can define how they want to establish and maintain a work-life balance.</li>
+                <li>53% increased confidence they can determine the steps they need to take to successfully accomplish their education and/or career goals</li>
+                <li>42% increased confidence they can choose a career that will match their work-life balance.</li>
+            </div>
+        </div>
+        <div class="layer-three">
+            <div class="grid-cell-one">
+                <img  src="<?= get_stylesheet_directory_uri() ?>/assets/images/eoy-2025/Confidence & Agency Pics/Image.png" alt="">
+                <div class='card-vertical' style="background-image: url('<?= get_stylesheet_directory_uri(); ?>/assets/images/eoy-2025/Backgrounds/BG-green.png');">card1</div>
+            </div>
+            <div class="grid-cell-two">
+                <img src="<?= get_stylesheet_directory_uri() ?>/assets/images/eoy-2025/Confidence & Agency Pics/Image (1).png" alt="">
+                <div class='card-horizontal' style="background-image: url('<?= get_stylesheet_directory_uri(); ?>/assets/images/eoy-2025/Backgrounds/BG-purple.png');">card2</div>
+            </div>
+            <div class="grid-cell-three">
+                <div class='card-horizontal' style="background-image: url('<?= get_stylesheet_directory_uri(); ?>/assets/images/eoy-2025/Backgrounds/BG-teal.png');">card3</div>
+                <img src="<?= get_stylesheet_directory_uri() ?>/assets/images/eoy-2025/Confidence & Agency Pics/Image (2).png" alt="">
+            </div>
+            <div class="grid-cell-four">
+                <img src="<?= get_stylesheet_directory_uri() ?>/assets/images/eoy-2025/Confidence & Agency Pics/Image (3).png" alt="">
+                <div class='card-vertical' style="background-image: url('<?= get_stylesheet_directory_uri(); ?>/assets/images/eoy-2025/Backgrounds/BG-gold.png');">card4</div>
+            </div>
         </div>
     </div>
 </section>
