e56b6f7e50f1ac3dcf0893bd4106c7ec0b7dd201
Joel Mariscal <jomarisc@ucsc.edu>
2025-12-03 13:36:55 -0800

    feat(hero):imported fonts

--- templates/template-parts/eoy-hero.php
+++ templates/template-parts/eoy-hero.php
@@ -37,8 +37,8 @@
             </div>
         </div>
         <div class="hero-description">
-            <h2>Your impact in 2025</h2>
-            <p>
+            <h2 class='ibm-plex-sans'>Your impact in 2025</h2>
+            <p class='nunito-sans'>
                 This is a pivotal time for Digital NEST. As we equip young people with 
                 the power to reshape their futures and create economic prosperity for 
                 families, we will become a national model for workforce development 
