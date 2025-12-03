e2fcb5fc6b8cca560e97eea32feab41c4510a961
Luciano Lorenzana <lucianolomalor251@gmail.com>
2025-12-03 14:36:22 -0800

    feat(TOC): Update element fonts

--- templates/template-parts/eoy-toc.php
+++ templates/template-parts/eoy-toc.php
@@ -1,12 +1,12 @@
 <section class="eoy-toc">
     <div class="container">
-        <h2>The future starts with you, <br>will you support the NEST generation?</h2>
+        <h2 class='ibm-plex-sans'>The future starts with you, <br>will you support the NEST generation?</h2>
 
-        <div class="button-container">
-            <button id="btn-mustard">Economic<br>Prosperity</button>
-            <button id="btn-violet">Confidence<br>& Agency </button>
-            <button id="btn-blue">A Workforce<br>Launchpad </button>
-            <button id="btn-cranberry">Financial<br>Foundation </button>
+        <div class="button-container ">
+            <button class='ibm-plex-sans' id="btn-mustard">Economic<br>Prosperity</button>
+            <button class='ibm-plex-sans' id="btn-violet">Confidence<br>& Agency </button>
+            <button class='ibm-plex-sans' id="btn-blue">A Workforce<br>Launchpad </button>
+            <button class='ibm-plex-sans' id="btn-cranberry">Financial<br>Foundation </button>
         </div>
 
         
