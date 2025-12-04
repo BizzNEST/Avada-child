a9c556f05af87992d8c0ce371b317606d4ad20df
NushK650 <Nush8653@gmail.com>
2025-12-04 11:13:56 -0800

    feat(economic) added font classes

--- templates/template-parts/eoy-economic.php
+++ templates/template-parts/eoy-economic.php
@@ -131,10 +131,10 @@ function getPeopleInVisualOrder() {
     <div class="container">
         <div class="header">
                 <img class="star" src="<?= get_stylesheet_directory_uri() ?>/assets/images/eoy-2025/Stars/Property 1=Default.png" alt="Financial Foundations Section 4">
-                <h2>Economic Prosperity</h2>
+                <h2 class='ibm-plex-sans'>Economic Prosperity</h2>
         </div>
         <div class="main-text">
-                <p>Of our 24-25 BizzNEST Associates, 79% secured paid next opportunities. Compare that to people looking for work in California, approximately 24% of whom found a job.* Digital NEST’s impact means young professionals are earning income, supporting families, and building wealth in communities historically excluded from economic opportunity.**</p>
+                <p class='nunito-sans'>Of our 24-25 BizzNEST Associates, 79% secured paid next opportunities. Compare that to people looking for work in California, approximately 24% of whom found a job.* Digital NEST’s impact means young professionals are earning income, supporting families, and building wealth in communities historically excluded from economic opportunity.**</p>
                 <ul>
                     <li>79% secured paid next opportunities</li>
                     <li>41% landed full or art-time jobs</li>
@@ -151,23 +151,23 @@ function getPeopleInVisualOrder() {
             <div class="stats">
                 <div class="fortyOne child">
                     <h3>41%</h3>
-                    <p>Landed full or part-time jobs</p>
+                    <p class='nunito-sans'>Landed full or part-time jobs</p>
                 </div>
                 <div class="thirtyThree child">
                     <h3>33%</h3>
-                    <p>Secured paid internships</p>
+                    <p class='nunito-sans'>Secured paid internships</p>
                 </div>
                 <div class="eleven child">
                     <h3>11%</h3>
-                    <p>Found contract work</p>
+                    <p class='nunito-sans'>Found contract work</p>
                 </div>
                 <div class="five child">
                     <h3>5%</h3>
-                    <p>Launched a venture</p>
+                    <p class='nunito-sans'>Launched a venture</p>
                 </div>
             </div>
         </div>
         <div class="subtext">
-            <p>*According to California EDD Labor Market Review August 2025. **These percentages add to more than 79% because some associates secured multiple opportunities.</p>
+            <p class='nunito-sans'>*According to California EDD Labor Market Review August 2025. **These percentages add to more than 79% because some associates secured multiple opportunities.</p>
         </div>
 </section>
