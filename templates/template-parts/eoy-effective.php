e680447213ef04477d3daf7b1781516ad3112fc8
Andre Maciel <andremaciel@Andres-MacBook-Pro.local>
2025-12-05 09:40:02 -0800

    fix(Animation):Making page on-view animation

--- templates/template-parts/eoy-effective.php
+++ templates/template-parts/eoy-effective.php
@@ -155,3 +155,24 @@
        </div>
    </div>
 </section>
+<script>
+    document.addEventListener("DOMContentLoaded", () => {
+
+    const targets = document.querySelectorAll(".bottom-section");
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("in-view");
+            } else {
+                entry.target.classList.remove("in-view");
+            }
+        });
+    }, {
+        threshold: 0.3 
+    });
+
+    targets.forEach(el => observer.observe(el));
+});
+
+</script>
